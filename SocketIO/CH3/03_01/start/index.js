037fcc024397f040a0c778dca83c235f422cda49
NORZACO AS <norzaco@gmail.com>
2023-01-16 17:43:37 +0100

    initial linkdin socket

---
+++ SocketIO/CH3/03_01/start/index.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// tech namespace
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('message', (msg) => {
+        console.log(`message: ${msg}`);
+        tech.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+
+        tech.emit('message', 'user disconnected');
+    })
+})
